ea03746c609be8f6cc482112f40c001d8822a908
GraveyQuen <31098956+GraveyQuen@users.noreply.github.com>
2024-07-05 12:32:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.58code.top": dockerHub,
+  "quay.58code.top": "https://quay.io",
+  "gcr.58code.top": "https://gcr.io",
+  "k8s-gcr.58code.top": "https://k8s.gcr.io",
+  "k8s.58code.top": "https://registry.k8s.io",
+  "ghcr.58code.top": "https://ghcr.io",
+  "cloudsmith.58code.top": "https://docker.cloudsmith.io",
+  "ecr.58code.top": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
